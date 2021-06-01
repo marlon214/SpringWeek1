d5d5d76ee5871c7d34764c37a7dbdea8bddc6ca6
Marlon <marlon.alejandro214@gmail.com>
2021-06-01 15:43:21 -0700

    Project push

---
+++ jeep-sales/src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -0,0 +1,40 @@
+package com.promineotech.jeep.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import com.promineotech.jeep.controller.support.FetchJeepTestSupport;
+import com.promineotech.jeep.entity.Jeep;
+import com.promineotech.jeep.entity.JeepModel;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@Sql(scripts = {
+    "classpath:flyway/migrations/V1.0__Jeep_Schema.sql",
+    "classpath:flyway/migrations/V1.1__Jeep_Data.sql"}, 
+    config = @SqlConfig(encoding = "utf-8"))
+
+class FetchJeepTest extends FetchJeepTestSupport{
+//    System.out.println(getBaseUri());
+  @Test
+  void testThatJeepsAreReturnedWhenAValidModelAndTrimAreSupplied() {
+    // Given: a valid model, trim and URI
+    JeepModel model = JeepModel.WRANGLER;
+    String trim = "Sport";
+    String uri = String.format("%s?model=%s&trim=%s", getBaseUri(), model, trim);
+    //When a connection is made to the URI
+    ResponseEntity<Jeep> response= getRestTemplate().getForEntity(uri, Jeep.class);
+    
+    // Then a 200 HTTP Status code is return 
+    
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+  }
+}
+  
